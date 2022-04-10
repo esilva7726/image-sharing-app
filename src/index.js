693122da5c5d11d240e84c5981c552ff604e3dea
silvaesteban7726@gmail.com <silvaesteban7726@gmail.com>
2022-04-10 14:04:51 -0300

    build: :tada: Instalacion de dependencias y creacion del server

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+// Initializations
+const app = express();
+
+// Settings
+
+// Middlewares
+
+// Global variables
+
+// Routes
+
+// Static files
+
+// Start the server
+app.listen(3000, () => {
+  console.log(`Server on port 3000`);
+});
