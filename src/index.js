dfd6135462b96d2d638204e0ff2540014b2db76e
silvaesteban7726@gmail.com <silvaesteban7726@gmail.com>
2022-04-10 14:17:05 -0300

    feat(server): :wrench: Agrego configuracion y middlewares al server

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const multer = require('multer');
 
 // Initializations
 const app = express();
 
 // Settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(multer({ dest: path.join(__dirname, 'public/img/upload') }).single('image'));
 
 // Global variables
+
 
 // Routes
 
 // Static files
 
 // Start the server
-app.listen(3000, () => {
-  console.log(`Server on port 3000`);
+app.listen(app.get('port'), () => {
+  console.log(`Server on port ${app.get('port')}`);
 });
