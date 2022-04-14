8db7dbb1b33697e9dc774389a2ebebba7eb38c25
silvaesteban7726@gmail.com <silvaesteban7726@gmail.com>
2022-04-13 21:32:02 -0300

    feat(image): :sparkles: Agrego vista para ver imagen y para eliminar imagen

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
 const router = Router();
+const { unlink } = require('fs-extra');
+const path = require('path');
 
 const Image = require('../models/image');
 
@@ -30,12 +32,17 @@ router.post('/upload', async (req, res) => {
   res.redirect('/');
 });
 
-router.get('/image/:id', (req, res) => {
-  res.send('Profile image');
+router.get('/image/:id', async (req, res) => {
+  const { id } = req.params;
+  const image = await Image.findById(id);
+  res.render('profile', { image });
 });
 
-router.delete('/image/:id/delete', (req, res) => {
-  res.send('Image deleted');
+router.get('/image/:id/delete', async (req, res) => {
+  const { id } = req.params;
+  const image = await Image.findByIdAndDelete(id);
+  await unlink(path.resolve('./src/public/' + image.path));
+  res.redirect('/');
 });
 
 module.exports = router;
